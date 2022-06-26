f908f2074a17f862a2cfede6e6e4cd58c1271ef7
Gia Minh <trangiaminh2002@gmail.com>
2022-06-26 14:11:47 +0700

    coded MemberDAO

--- DataAccess/DataAccessObjects/Contexts/MemberDAO.cs
+++ DataAccess/DataAccessObjects/Contexts/MemberDAO.cs
@@ -2,13 +2,14 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccess.DataAccessObjects.Contexts;
 
-public class MemberDAO
+public class MemberDAO : BaseDAO
 {
     private static MemberDAO instance = null;
     private static readonly object instanceLock = new object();
@@ -32,24 +33,168 @@ public class MemberDAO
         IDataReader dataReader = null;
         var memberList = new List<Member>();
         // CONTINUE CODE HERE
+        string sql = "select * from Member";
+        try
+        {
+            dataReader = dataAccessProvider.GetDataReader(
+                sql,
+                CommandType.Text,
+                out connection);
+            while (dataReader.Read())
+            {
+                memberList.Add(new Member
+                {
+                    MemberID = dataReader.GetInt32(0),
+                    MemberName = dataReader.GetString(1),
+                    Email = dataReader.GetString(2),
+                    Password = dataReader.GetString(3),
+                    City = dataReader.GetString(4),
+                    Country = dataReader.GetString(5),  
+                });
+            }
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+        finally
+        {
+            dataReader.Close();
+            CloseConnection();
+        }
         return memberList;
     }
     public Member GetMemberById(int memberId)
     {
         Member member = null;
         // CONTINUE CODE HERE
+        IDataReader dataReader = null;
+        string sql = "select * from Member where MemberId=@MemberId";
+        try
+        {
+            var param = dataAccessProvider.CreateParameter(
+                "@MemberId",
+                4,
+                memberId,
+                DbType.Int32);
+            dataReader = dataAccessProvider.GetDataReader(
+                sql,
+                CommandType.Text,
+                out connection,
+                param);
+            if (dataReader.Read())
+            {
+                member = new Member
+                {
+                    MemberID = dataReader.GetInt32(0),
+                    MemberName = dataReader.GetString(1),
+                    Email = dataReader.GetString(2),
+                    Password = dataReader.GetString(3),
+                    City = dataReader.GetString(4),
+                    Country = dataReader.GetString(5),
+                };
+            }
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+        finally
+        {
+            CloseConnection();
+        }
         return member;
     }
     public void InsertMember(Member member)
     {
         // CODE HERE
+        string sql = "insert Member values(@MemberId, @MemberName, " +
+            "@Email, @Password, @City, @Country)";
+        try
+        {
+            Member checkMember = GetMemberById(member.MemberID);
+            if (checkMember == null)
+            {
+                var param = new List<SqlParameter>();
+                param.Add(dataAccessProvider.CreateParameter("@MemberName", 50, member.MemberName, DbType.String));
+                param.Add(dataAccessProvider.CreateParameter("@Email", 50, member.Email, DbType.String));
+                param.Add(dataAccessProvider.CreateParameter("@Password", 30, member.Password, DbType.String));
+                param.Add(dataAccessProvider.CreateParameter("@City", 20, member.City, DbType.String));
+                param.Add(dataAccessProvider.CreateParameter("@Country", 20, member.Country, DbType.String));
+                dataAccessProvider.Insert(sql, CommandType.Text, param.ToArray());
+            }
+            else
+            {
+                throw new Exception("Member already exist!");
+            }
+        } 
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+        finally
+        {
+            CloseConnection();
+        }
     }
     public void UpdateMember(Member member)
     {
         // CODE HERE
+        string sql = "update Member set MemberName=@MemberName, Email=@Email, " +
+            "Password=@Password, City=@City, Country=@Country where MemberId=@MemberId";
+        try
+        {
+            Member checkMember = GetMemberById(member.MemberID);
+            if (checkMember != null)
+            {
+                var param = new List<SqlParameter>();
+                param.Add(dataAccessProvider.CreateParameter("@MemberId", 4, member.MemberID, DbType.Int32));
+                param.Add(dataAccessProvider.CreateParameter("@MemberName", 50, member.MemberName, DbType.String));
+                param.Add(dataAccessProvider.CreateParameter("@Email", 50, member.Email, DbType.String));
+                param.Add(dataAccessProvider.CreateParameter("@Password", 30, member.Password, DbType.String));
+                param.Add(dataAccessProvider.CreateParameter("@City", 20, member.City, DbType.String));
+                param.Add(dataAccessProvider.CreateParameter("@Country", 20, member.Country, DbType.String));
+                dataAccessProvider.Update(sql, CommandType.Text, param.ToArray());
+            }
+            else
+            {
+                throw new Exception("Member does not exist!");
+            }
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+        finally
+        {
+            CloseConnection();
+        }
     }
     public void RemoveMember(int memberId)
     {
         // CODE HERE
+        string sql = "delete Member where MemberId=@MemberId";
+        try
+        {
+            Member checkMember = GetMemberById(memberId);
+            if (checkMember != null)
+            {
+                var param = new List<SqlParameter>();
+                param.Add(dataAccessProvider.CreateParameter("@MemberId", 4, memberId, DbType.Int32));
+                dataAccessProvider.Update(sql, CommandType.Text, param.ToArray());
+            }
+            else
+            {
+                throw new Exception("Member does not exist!");
+            }
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+        finally
+        {
+            CloseConnection();
+        }
     }
 }
