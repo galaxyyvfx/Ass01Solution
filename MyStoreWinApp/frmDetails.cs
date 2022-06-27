e9238f3b8e590b93e1600104cce02008d0b44136
Ngoc Toan <ksgalaxyy@gmail.com>
2022-06-27 10:33:50 +0700

    - Remove unnecessary usings

--- MyStoreWinApp/frmDetails.cs
+++ MyStoreWinApp/frmDetails.cs
@@ -1,14 +1,4 @@
 ﻿using BusinessObject;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using BusinessObject.Services;
 using DataAccess.Entities;
 
 namespace MyStoreWinApp
